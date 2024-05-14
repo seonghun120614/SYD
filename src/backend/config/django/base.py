129150d59f40bd1137c29a321c89c7096134e3bf
seonghun120614 <seonghun120614@gmail.com>
2024-05-15 00:15:59 +0900

    chore: Create  for environment variables

--- src/backend/config/django/base.py
+++ src/backend/config/django/base.py
@@ -1,17 +1,4 @@
-from pathlib import Path
-
-import os
-import environ
-
-env = environ.Env()
-BASE_DIR = environ.Path(__file__) - 3
-APPS_DIR = BASE_DIR.path("apps")
-
-environ.Env().read_env(os.path.join(BASE_DIR, '.env'))
-
-SECRET_KEY = env('SECRET_KEY', default="t*&i$9@452mcsqp%ud8s8@!yrh^f!wr9_n)j!&z0978!=_nu-a")
-DEBUG = True
-ALLOWED_HOSTS = ["*"]
+from config.env import *
 
 
 # Application definition
---
+++ src/backend/config/env.py
@@ -0,0 +1,14 @@
+import environ
+import os
+
+
+env = environ.Env()
+
+BASE_DIR = environ.Path(__file__) - 2
+APPS_DIR = BASE_DIR.path("apps")
+
+environ.Env().read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY', default="t*&i$9@452mcsqp%ud8s8@!yrh^f!wr9_n)j!&z0978!=_nu-a")
+
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
