c4fa1f790f62964ada1a8398b54b839374253fed
seonghun120614 <seonghun120614@gmail.com>
2024-06-09 15:32:44 +0900

    fix: Revise access entry in back server

--- back/config/django/local.py
+++ back/config/django/local.py
@@ -6,6 +6,5 @@ SECRET_KEY = "afidz$4l_e$&p#9-(ago-e4=38(@=za(aaik=qbmg4i4u=eap)"
 
 # Allow Hosts for Cross-origin resource sharing
 CORS_ALLOWED_ORIGINS = (
-  'http://localhost:5173',
-  'http://localhost:3000'
+  'http://0.0.0.0:80'
 )
