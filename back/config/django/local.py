9ed7b2f59df6c1cdb0298dd9602d51c7172c15ff
seonghun120614 <seonghun120614@gmail.com>
2024-06-09 15:35:55 +0900

    fix: Resolve wrong

--- back/config/django/local.py
+++ back/config/django/local.py
@@ -6,5 +6,5 @@ SECRET_KEY = "afidz$4l_e$&p#9-(ago-e4=38(@=za(aaik=qbmg4i4u=eap)"
 
 # Allow Hosts for Cross-origin resource sharing
 CORS_ALLOWED_ORIGINS = (
-  'http://0.0.0.0:80'
+  '0.0.0.0:80'
 )
