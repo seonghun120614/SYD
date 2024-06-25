4cafa059d09d2471e10b1282665fa562948043ac
seonghun120614 <seonghun120614@gmail.com>
2024-06-25 16:00:17 +0900

    chore: Divide production environment and develop environment

--- back/config/django/prod.py
+++ back/config/django/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [ "*" ]
 
 CORS_ALLOWED_ORIGINS = (
--- front/vite.config.js
+++ front/vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:8000',
         target: 'http://host.docker.internal:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
