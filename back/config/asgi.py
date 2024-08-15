fbffa03a92513da74559018953fbbc1ec97a1aec
seonghun120614 <seonghun120614@gmail.com>
2024-08-15 14:10:48 +0900

    chore: revise for deployment

--- back/config/asgi.py
+++ back/config/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.prod')
 
 application = get_asgi_application()
--- back/config/env.py
+++ back/config/env.py
@@ -2,6 +2,10 @@ import environ
 import os
 
 # Setup Root and Apps Directory
+env = environ.Env(
+    DEBUG=(bool, True)
+)
+
 BASE_DIR = APPS_DIR = environ.Path(__file__) - 2
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
