9ddea1469b88743b291dc5a63e383cfe6a41649c
seonghun120614 <seonghun120614@gmail.com>
2024-08-15 16:12:23 +0900

    chore: Fix configuration for develop

--- back/config/django/local.py
+++ back/config/django/local.py
@@ -5,10 +5,6 @@ ALLOWED_HOSTS = [ "*" ]
 CORS_ALLOWED_ORIGINS = (
     'http://0.0.0.0:8080',
     'http://0.0.0.0:80',
-    'http://localhost:8080',
-    'http://localhost:80',
-    'http://syd-front:8080',
-    'http://syd-front:80',
 )
 
 # Database config
--- front/vite.config.js
+++ front/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://syd-back:8000/api',
+        target: 'http://back:8000/api/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
